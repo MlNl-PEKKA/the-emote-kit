6d535d9fd8d5f1d874fd04fe831d45dc9159145a
MlNl-PEKKA <punithg360@gmail.com>
2024-12-07 14:18:39 +0530

    sample widget

---
+++ apps/widget/src/index.tsx
@@ -0,0 +1,3 @@
+import WidgetWebComponent from "./widget-web-component";
+
+customElements.define("my-widget", WidgetWebComponent);
---
+++ apps/widget/src/widget-web-component.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import ReactDom from "react-dom/client";
+import { Widget } from "./Widget";
+
+export const normalizeAttribute = (attribute: string) => {
+  return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+};
+
+class WidgetWebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const props = this.getPropsFromAttributes();
+    const root = ReactDom.createRoot(this.shadowRoot!);
+    root.render(<Widget {...props} />);
+  }
+
+  getPropsFromAttributes() {
+    const props: Record<string, unknown> = {};
+    for (const { name, value } of this.attributes) {
+      props[normalizeAttribute(name)] = value;
+    }
+    return props;
+  }
+}
+
+export default WidgetWebComponent;
