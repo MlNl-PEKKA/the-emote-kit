8a2592e38e454484615c7c13cdc82ada40e4cf79
MlNl-PEKKA <punithg360@gmail.com>
2024-12-08 17:51:01 +0530

    update vite config

--- apps/widget/vite.config.ts
+++ apps/widget/vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  define: {
+    "process.env": {
+      NODE_ENV: "production",
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
